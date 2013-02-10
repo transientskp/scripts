3196ce2d3485d8bc4c2295afddfbf6dc26fbf66a
AntoniaR <b.a.rowlinson@uva.nl>
2013-02-10 15:51:11 +0100

    To identify unique transients and their associations, and reate a DS9 region file.

--- find_flash/assoc_trans.py
+++ find_flash/assoc_trans.py
@@ -17,21 +17,29 @@ def main(opts,args):
     lines=f.readlines()
     f.close()
     f2 = open('transients.txt','a')
+    f3 = open('transients.reg','w')
+    f3.write('global color=green dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1\n fk5\n')
+    assoc_sources=[]
     for line in lines:
         assoc=[]
         n=0
         info=line.split(' ')
-        x1 = C.Position((float(info[1]), float(info[2])))
-        for line2 in lines:
-            info2=line2.split(' ')
-            x2 = C.Position((float(info2[1]), float(info2[2])))
-            posdif = x1.angsep(x2)
-            if posdif < max_sep_cutoff:
-                if info[0] != info2[0]:
-                    assoc.append(info2[0])
-        if assoc:
-            f2.write('Transients associated! '+str(info[0])+' and '+str(", ".join(assoc))+'\n')
+        if info[0] not in assoc_sources:
+            assoc_sources.append(info[0])
+            x1 = C.Position((float(info[1]), float(info[2])))
+            for line2 in lines:
+                info2=line2.split(' ')
+                x2 = C.Position((float(info2[1]), float(info2[2])))
+                posdif = x1.angsep(x2)
+                if posdif < max_sep_cutoff:
+                    if info[0] != info2[0] and int(info[0]) < int(info2[0]):
+                        assoc.append(info2[0])
+                        assoc_sources.append(info2[0])
+#            if assoc:
+            f2.write('Unique transients: '+str(info[0])+', '+str(", ".join(assoc))+'\n')
+            f3.write('circle('+info[1]+','+info[2]+',200") # width=3\n')
     f2.close()
+    f3.close()
 
 opt = optparse.OptionParser()
 opt.add_option('-s','--max_sep_cutoff',help='The maximum separation allowed for associated sources (degrees)',default='0.05',type='float')
